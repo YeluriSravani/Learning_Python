3b7f34aee08acc9a7d628b0cfd64de2c2273513b
YeluriSravani <130227924+YeluriSravani@users.noreply.github.com>
2023-04-08 19:19:54 +0530

    Create common_characters.py

---
+++ common_characters.py
@@ -0,0 +1,24 @@
+#Write a python program to display all the common characters between two strings. Return -1 if there are no matching characters.
+
+def find_common_characters(msg1,msg2):
+    #pass #Remove pass and write your logic here
+    common=''
+    for i in msg1:
+        for j in msg2:
+            if i==j:
+               if i not in common:
+                  common+=i
+               else:
+                  continue
+            else:
+               continue
+    
+    if common=='':
+       common=-1
+    return common
+
+#Provide different values for msg1,msg2 and test your program
+msg1="I like Python"
+msg2="Java is a very popular language"
+common_characters=find_common_characters(msg1,msg2)
+print(common_characters)
