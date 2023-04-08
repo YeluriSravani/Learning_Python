22c2a1cc5ecb8ed61a72943f287154fe38e564f8
YeluriSravani <130227924+YeluriSravani@users.noreply.github.com>
2023-04-08 19:44:57 +0530

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,34 @@
+#Write a python function, encrypt_sentence() which accepts a message and encrypts it based on rules given below and returns the encrypted message.
+#Words at odd position -> Reverse It
+#Words at even position -> Rearrange the characters so that all consonants appear before the vowels and their order should not change
+
+def encrypt_sentence(sentence):
+    #start writing your code here
+    list=[]
+    flag='n'
+    encrypt=''
+    vowel=''
+    word1=''
+    list=sentence.split(' ')
+    for word in list:
+        if flag=='y':
+           encrypt+=' '
+        if (list.index(word)+1)%2==0:
+           word1=''
+           vowel=''
+           for i in range(0,len(word)):
+               if word[i] in ['a','e','i','o','u']:
+                  vowel+=word[i]
+               else:
+                  word1+=word[i]
+           
+           encrypt+=word1+vowel
+        else:
+           encrypt+=word[::-1]
+        
+        flag='y'
+    return encrypt 
+
+sentence="The sun rises in the east"
+encrypted_sentence=encrypt_sentence(sentence)
+print(encrypted_sentence)
